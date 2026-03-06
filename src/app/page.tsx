90c5126f81dc9ea95c2dbbfa11be7fab1ed2d72a
rojolocco <rojolocco@gmail.com>
2026-03-05 23:02:28 -0500

    ui styles added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
-        <h1>HELLO WORLD!</h1>
-      </main>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-bold">HELLO WORLD!</h1>
     </div>
   );
 }
